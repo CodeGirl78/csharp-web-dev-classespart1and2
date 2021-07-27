08f325e860e08dfbc6af7532fc454e8c90ef7f35
Melinda <codegirl78@gmail.com>
2021-07-27 18:53:22 -0500

    instantiated the Student class using myself as a student

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,7 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student melinda = new Student("Melinda Surmeier", 001, 1, 4.0);
         }
     }
 }
