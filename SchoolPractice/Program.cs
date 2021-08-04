46e1a95ae075dda8e962a9ee396f0c502ffb8981
Melinda <codegirl78@gmail.com>
2021-08-04 15:38:26 -0500

    Completed exercises

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -9,15 +9,14 @@ namespace SchoolPractice
         {
             // TODO: Test your exercise solutions with print statements here.
             Student myStudent1 = new Student("Melinda Surmeier", 001, 1, 4.0);
-
-            //myStudent1.AddGrade(3, 3.4);
+            //myStudent1.AddGrade(3, 3.5);
             //Console.WriteLine(myStudent.Gpa);
             Console.WriteLine(myStudent1.ToString());
 
-            Student myStudent2 = new Student("Michelle Surmeier", 001, 1, 4.0);
+            Student myStudent2 = new Student("Michelle Surmeier", 001, 1, 3.8);
             myStudent1.Equals(myStudent2);
 
-            Teacher myTeacher1 = new Teacher("Chris", "Surmeier", "WebDev", 30);
+            Teacher myTeacher1 = new Teacher("Chris", "Surmeier", "WebDev", 20);
 
             List<Student> myStudents = new List<Student>();
             myStudents.Add(myStudent1);
